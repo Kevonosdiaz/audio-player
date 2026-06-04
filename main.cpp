f37e9362f0c8c5ed0eefbefd5c5930d6e12ca18e
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2026-06-04 16:48:13 -0600

    add basic play/pause to test libmpdclient connection

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include "mainwindow.h"
+#include <QDebug>
+#include <mpd/client.h>
+#include <iostream>
+#include <memory>
 
 #include <QApplication>
 
@@ -7,5 +11,30 @@ int main(int argc, char* argv[])
     QApplication a(argc, argv);
     MainWindow   w;
     w.show();
+
+    // Try simple MPD connection to test basic functionality
+    struct mpd_connection* conn = mpd_connection_new(NULL, 0, 0);
+
+    // Error handling
+    if (conn == nullptr) {
+        std::cerr << "OOM" << std::endl;
+        return 1;
+    }
+
+    if (mpd_connection_get_error(conn) != MPD_ERROR_SUCCESS) {
+        std::cerr << "Connection error: " << mpd_connection_get_error_message(conn) << std::endl;
+        mpd_connection_free(conn);
+        return 1;
+    }
+
+    // Try play/pause
+    if (!mpd_run_toggle_pause(conn)) {
+        std::cerr << "Failed to toggle playback" << std::endl;
+    } else {
+        std::cout << "Successfully toggled playback" << std::endl;
+    }
+
+    mpd_connection_free(conn);
+
     return QApplication::exec();
 }
