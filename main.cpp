faf8266321b7705adccfe5f23c1966f426f29b6e
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2026-06-10 11:45:31 -0600

    move MpdHandler object onto worker thread

--- main.cpp
+++ main.cpp
@@ -9,21 +9,19 @@ int main(int argc, char* argv[])
     QApplication a(argc, argv);
 
     // Setup second thread and run MPD related tasks on it
-    // QThread*    mpd_thread  = new QThread();
+    QThread    mpd_thread;
     MpdHandler mpd_handler;
-    // mpd_handler.moveToThread(mpd_thread);
+    mpd_handler.moveToThread(&mpd_thread);
 
     // Delete worker after thread is finished
-    // QObject::connect(mpd_thread, &QThread::finished, &mpd_handler, &QObject::deleteLater);
+    QObject::connect(&mpd_thread, &QThread::finished, &mpd_handler, &QObject::deleteLater);
 
     // Setup connections so worker can be told what to do
 
-    // mpd_thread->start();
+    mpd_thread.start();
 
     MainWindow w(&mpd_handler);
     w.show();
-
-    // delete mpd_thread;
 
     return QApplication::exec();
 }
